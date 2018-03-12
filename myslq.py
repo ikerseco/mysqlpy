740e61330a97bfcacc578a2194dfd6ad1f4550c8
iker <33206796+ikerseco@users.noreply.github.com>
2018-03-12 03:21:46 +0100

    select bukatuak

---
+++ myslq.py
@@ -0,0 +1,37 @@
+import pymysql.cursors
+
+print("Zure datubasera konektatzen:\n")
+dbi = input("\t*jarri zure databasearen izena:")
+useri = input("\t*user:")
+passwordi = input("\t*password:")
+hosti = input("\t*host:") 
+# Connect to the database
+connection = pymysql.connect(host=hosti,
+                             user=useri,
+                             password=passwordi,
+                             db=dbi,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+try:
+    # select bitarteko erantzunak
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM `employees`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(len(result))
+        for x in range(len(result)):
+         print (result[x])
+         print(result[x]['firstname'])
+
+
+    #tablak ikustatu
+    with connection.cursor() as cursor:
+        sql = "describe employees"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(len(result))
+        for x in range(len(result)):
+            print(result[x])
+finally:
+    connection.close()

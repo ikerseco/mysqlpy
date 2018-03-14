7f73caeabb23cd0f94428fe26627bf09876e381f
iker <33206796+ikerseco@users.noreply.github.com>
2018-03-14 02:20:18 +0100

    claseak sortuak

--- myslq.py
+++ myslq.py
@@ -1,5 +1,5 @@
 import pymysql.cursors
-
+"""
 print("Zure datubasera konektatzen:\n")
 dbi = input("\t*jarri zure databasearen izena:")
 useri = input("\t*user:")
@@ -35,3 +35,40 @@ try:
             print(result[x])
 finally:
     connection.close()
+"""
+class bistaratu(object):
+    def __init__(self, user, password, db, host):
+        self.user = user
+        self.password = password
+        self.db = db
+        self.host = host
+        self.conec = None
+
+    def conexioa(self):
+        connection = pymysql.connect(host = self.host,
+                                     user = self.user,
+                                     password = self.password,
+                                     db = self.db,
+                                     charset = 'utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        self.conec = connection
+    def ikusi(self):
+        with self.conec.cursor() as cursor:
+         sql = "show tables"
+         cursor.execute(sql)
+         result = cursor.fetchall()
+         print(len(result))
+         for x in range(len(result)):
+             print(result[x])                                 
+   
+       
+
+print("Zure datubasera konektatzen:\n")
+db = input("\t*jarri zure databasearen izena:")
+user = input("\t*user:")
+password = input("\t*password:")
+host = input("\t*host:") 
+
+mysql = bistaratu( user, password, db, host)
+mysql.conexioa()
+mysql.ikusi()

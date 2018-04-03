95ebcd168ac7f269dec2211fbed14772f19133a0
iker <33206796+ikerseco@users.noreply.github.com>
2018-04-03 03:54:59 +0200

    create table

--- myslq.py
+++ myslq.py
@@ -24,8 +24,6 @@ try:
         for x in range(len(result)):
          print (result[x])
          print(result[x]['firstname'])
-
-
     #tablak ikustatu
     with connection.cursor() as cursor:
         sql = "describe employees"
@@ -68,16 +66,21 @@ class data_base(object):
             result = cursor.fetchall()
             return result   
     
-    def create_t(self,json,t_izena):
+    def create_t(self,datuak,t_izena):
         with self.conec.cursor() as cursor:
             try:
-                cursor.execute(json)
+                print(self.db)
+                print(datuak)
+                print(t_izena)
+                data = "CREATE TABLE "+t_izena+ "( " + datuak + ") ENGINE = InnoDB;"
+                print(data)
+                cursor.execute(data)
                 return("zure tabla sortu egin da zorionak!!!")
             except ValueError:               
                 return("arazo bat egon da zure tabla sortzean!!!")
 
 #json
-data = [{'izena':'name', 'valioa':'bai','Funtzioa':'null'},{'izena':'atributoa','valioa': 'bai','Funtzioa':'null'},{'izena':'NUll','valioa':'bai','Funtzioa':'null'},{'izena':'atributoa','valioa':'null','Funtzioa':'null'}]
+data = [{'izena':'name','Funtzioa':''},{'izena':'atributoa','Funtzioa':''},{'izena':'NUll','Funtzioa':''},{'izena':'DEFAULT','Funtzioa':''}]
 #funtzioak
 def atributo(izen):
     atri = [{'1':'INT','2':'VARCHAR','3':'TEXT','4':'DATE'}] 
@@ -95,6 +98,9 @@ def defektuzko(izen):
     if izen == "2":
       data[3]['valioa'] = "bai"
       return "CURRENT_TIMESTAMP"
+
+
+
 #programa
 print("Zure datubasera konektatzen:\n")
 db = input("\t*jarri zure databasearen izena:")
@@ -115,10 +121,13 @@ while True:
  print("\n")
  if int(zent) == len(tabla_s):
   izen_tabla = input("jarri tablaren izena:")
-  kanti = input("zen bat zutabe:")
+  kanti = input("zenbat zutabe:")
+  zutabeak = ""
+  pry = ""
   for x in range(int(kanti)):
      print("\n")
      izen_zutabea = input("jarri zutabearen izena:")
+     data[0]['Funtzioa'] = izen_zutabea
      print("\n")
      print("-Aukeratu atributo bat:")
      print("\t1.INT")
@@ -136,7 +145,6 @@ while True:
      print("\t1.BAI")
      print("\t2.EZ")
      aukerNL = input("aukera:")
-    # data[2]['valioa'] = "bai"
      n = Null(aukerNL)
      data[2]['Funtzioa'] = n
      print("\n")
@@ -145,11 +153,27 @@ while True:
      print("\t2.CURRENT_TIMESTAMP")
      print("\t3.ez")
      aukerDF = input("aukera:")
-     d = defektuzko(aukerDF)
-     data[3]['Funtzioa'] = d
+     if aukerDF != "3":
+      d = defektuzko(aukerDF)
+      print(d)
+      data[3]['Funtzioa'] = d
      print("\n")
+     zutabeak += " " + data[0]['Funtzioa'] +" "+ data[1]['Funtzioa'] +" "+ data[2]['Funtzioa'] +" "+ data[3]['Funtzioa'] +","
+     pry += "\t*"+data[0]['Funtzioa'] + "\n"
+  print("-Prymari key:")
+  print("\t1.BAI")
+  print("\t2.EZ")
+  a = input("aukera:")
+  if a == "1":
+      print("zutabeak:")
+      print(pry)
+      zu_name = input("zutabearen izena:")
+  print("\n")
+  zutabeak += "PRIMARY KEY("+zu_name+")"
+  print(zutabeak)
+  print(izen_tabla)
+  mysql.create_t(zutabeak,izen_tabla)
 
-      
  else:
   print("ze nahi duzu?")
   print("\t1.visual")
@@ -161,13 +185,8 @@ while True:
      tabla_des = mysql.describe_t(tabla_s[int(zent)]['Tables_in_python'])
      print("\n")
      print("Zutabeak:")
-     print(tabla_des)
+     #print(tabla_des)
      for x in range(len(tabla_des)):
          print("\t",[x],".Izena:",tabla_des[x]["Field"],"; Karaktere_Mota:",tabla_des[x]["Type"],"; NULL:",tabla_des[x]["Null"],"; Giltza:",tabla_des[x]["Key"],"; Defektuzko_izena:",tabla_des[x]["Default"],"; Gehigarria:",tabla_des[x]["Extra"])
      print("\t",[len(tabla_des)],".sortu zutabe berri bat:")
      print("\n")
-
-
-
- 
-
